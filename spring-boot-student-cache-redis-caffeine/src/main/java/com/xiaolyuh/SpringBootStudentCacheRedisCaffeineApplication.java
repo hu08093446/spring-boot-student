90361b489b45df01043f7cdd52b9becf94118f4f
huke <1406308862@qq.com>
2018-07-22 19:56:39 +0800

    18-07-22 测试git能否提交使用

--- spring-boot-student-cache-redis-caffeine/src/main/java/com/xiaolyuh/SpringBootStudentCacheRedisCaffeineApplication.java
+++ spring-boot-student-cache-redis-caffeine/src/main/java/com/xiaolyuh/SpringBootStudentCacheRedisCaffeineApplication.java
@@ -12,5 +12,6 @@ public class SpringBootStudentCacheRedisCaffeineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStudentCacheRedisCaffeineApplication.class, args);
+		System.out.println("我就试试能不能提交");
 	}
 }
